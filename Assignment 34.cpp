2f7ed7ff14c0311c69c070b16bce102f5d5a8b76
MoulishaChoudhury29 <choudhurymoulisha@gmail.com>
2025-02-24 15:53:37 +0530

    Add files via upload

---
+++ Assignment 34.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,m,a,x=0;
+	printf("Enter the no. ");
+	scanf("%d", &n);
+	m=n;
+	while(n>0)
+	{
+		a=n%10;
+		x=x+pow(a,3);
+		n=n/10;
+	}
+	if(m==x)
+	printf("Armstrong number");
+	else
+	printf("Not armstrong number");
+}
